18b4c5e0ee118174bec194d4bffdf423c8248b6e
NamanPrakash99 <namanprakash2580@gmail.com>
2025-07-15 16:29:59 +0530

    Added te create rrom functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div className="container text-center py-5">
+            <h2 className="mb-2">SmartQA - Get Started!</h2>
+            <p className="mb-1">
+                Click on Create Room if you are the host to get started.
+                Share the code with participants.
+            </p>
+
+            <p className="mb-4">
+                If you are a participant, the click on Join Room.
+                Ask for room code from the host of the meeting.
+            </p>
+
+
+            <Link to="/create" className="btn btn-primary me-1">
+                Create Room
+            </Link>
+            <Link to="/join" className="btn btn-success">
+                Join Room
+            </Link>
+        </div>
+    );
+}
+
+export default Home;
